03aa2d62d067de40b01cbf27c2644d6570c55583
KURS HOMEWORK <kreshvan12317@yandex.ru>
2024-11-30 21:13:28 +0300

    РАБОЧАЯ ВЕТКА

--- src/main/java/groupId/ru/hogwarts/school/exepcion/StudentNotFoundException.java
+++ src/main/java/groupId/ru/hogwarts/school/exepcion/StudentNotFoundException.java
@@ -1,16 +1,1 @@
-package groupId.ru.hogwarts.school.exepcion;
 
-import groupId.ru.hogwarts.school.model.Student;
-
-public class StudentNotFoundException extends RuntimeException {
-    public StudentNotFoundException(long id) {
-
-        super("Student not found by id [%s]".formatted(id));
-    }
-
-    public StudentNotFoundException(Class<?> clazz) {
-        super("Student not found by id [%s]".formatted(clazz.getSimpleName()));
-    }
-
-
-}
